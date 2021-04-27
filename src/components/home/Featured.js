f8885a36e8b3e5378675635408e4eef896a0b16d
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-04-28 06:44:43 +0700

    feat: add featured component on home page

---
+++ src/components/home/Featured.js
@@ -0,0 +1,66 @@
+import classes from "./Featured.module.css";
+
+export default function Featured() {
+  return (
+    <div className="bg-white">
+      <section className={`container ${classes.featured}`}>
+        <div className={classes.illustration}>
+          <img
+            className={classes["img-illustration"]}
+            src="/assets/images/featured.svg"
+            alt="featured"
+          />
+        </div>
+        <div className={classes["perks-container"]}>
+          <h2 className={classes.title}>
+            We Provide Good Coffee and Healthy Meals
+          </h2>
+          <p className={classes.description}>
+            You can explore the menu that we provide with fun and have their own
+            taste and make your day better.
+          </p>
+          <p className={classes.perks}>
+            <img
+              className={classes.checklist}
+              src="/assets/icons/perks-checklist.svg"
+              alt="checklist"
+            />
+            High quality beans
+          </p>
+          <p className={classes.perks}>
+            <img
+              className={classes.checklist}
+              src="/assets/icons/perks-checklist.svg"
+              alt="checklist"
+            />
+            High quality beans
+          </p>
+          <p className={classes.perks}>
+            <img
+              className={classes.checklist}
+              src="/assets/icons/perks-checklist.svg"
+              alt="checklist"
+            />
+            Healthy meals, you can request the ingredients
+          </p>
+          <p className={classes.perks}>
+            <img
+              className={classes.checklist}
+              src="/assets/icons/perks-checklist.svg"
+              alt="checklist"
+            />
+            Chat with our staff to get better experience for ordering
+          </p>
+          <p className={classes.perks}>
+            <img
+              className={classes.checklist}
+              src="/assets/icons/perks-checklist.svg"
+              alt="checklist"
+            />
+            Free member card with a minimum purchase of IDR 200.000.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,15 @@
 import Hero from "../../components/home/Hero";
 import Stats from "../../components/home/Stats";
+import Featured from "../../components/home/Featured";
 
-import classes from "./Home.module.css";
+// import classes from "./Home.module.css";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Stats />
+      <Featured />
     </main>
   );
 }
