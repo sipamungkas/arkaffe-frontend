473fd68a41b73f777a81b4af74b239352444d4f9
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-04-27 23:27:49 +0700

    fix: bootstrap import js error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./index.css";
 import App from "./App";
 
