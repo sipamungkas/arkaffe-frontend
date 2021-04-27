11ef6286c2be3c5d0b9f034a87a5fb169ecf08c8
Riski Arifani <rskrfni@gmail.com>
2021-04-27 10:56:39 +0700

    chore: Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { homepage } from "./pages/HomePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={homepage}></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export class homepage extends Component {
+  render() {
+    return (
+      <>
+        <header>
+          {/* Header */}
+          <h2>Header Section</h2>
+        </header>
+        <main>
+          {/* Main Content */}
+          <h2>Main Content</h2>
+        </main>
+        <footer>
+          {/* Footer */}
+          <h2>Footer Section</h2>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default homepage;
