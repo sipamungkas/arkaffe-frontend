97efe5278d656d34ef045d724f81f8cada8d8c4e
Riski Arifani <rskrfni@gmail.com>
2021-04-28 23:13:42 +0700

    feat: Forgot Password

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Switch, Route } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Login from "./pages/authorization/login/Login";
-import SignUp from "./pages/authorization/SignUp";
+import SignUp from "./pages/authorization/signup/SignUp";
+import ForgotPassword from "./pages/authorization/forgot/ForgotPassword"
 import Home from "./pages/home/Home";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login}></Route>
         <Route path="/signup" component={SignUp}></Route>
+        <Route path="/login/recover" component={ForgotPassword}></Route>
       </Switch>
     </Layout>
   );
