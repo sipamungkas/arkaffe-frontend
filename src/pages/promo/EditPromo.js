a997f1d778be80c60bb451bbf34c736820f8bd3c
Riski Arifani <rskrfni@gmail.com>
2021-05-03 23:22:45 +0700

    feat: Edit Promo

--- src/pages/promo/EditPromo.js
+++ src/pages/promo/EditPromo.js
@@ -2,16 +2,24 @@ import React from "react";
 import classes from "./EditPromo.module.css";
 import BreadCrumb from "../../components/breadcrumb/BreadCrumbV2";
 import ProductPicture from "../../components/product/newproduct/leftsection/ProductPicture"
-import LeftInput from "../../components/product/promo/newpromo/LeftInput"
-import RightInput from "../../components/product/promo/newpromo/RightInput";
+import LeftInput from "../../components/product/promo/editpromo/LeftInput"
+import RightInput from "../../components/product/promo/editpromo/RightInput";
+import PromoCard from "../../components/product/promo/editpromo/PromoCard";
 
 function EditPromo() {
+    const name = "Beef Spaghetti"
+    const discount = "20"
+    const desc = "Buy 1 Choco Oreo and get 20% off for Beef Spaghetti"
+    const couponcode = "fnpr15rg"
+    const valid = `${"October"}${" "}${10}th ${2020}`
+
+    const Promo = {name, discount, desc, couponcode, valid}
   return (
     <div>
       <main className={classes["main-container"]}>
         <section className={classes["left-container"]}>
           <BreadCrumb path={["Promo", "Edit Promo"]} />
-          <ProductPicture />
+          <PromoCard promo={Promo}/>
           <LeftInput />
         </section>
         <section className={classes["right-container"]}>
