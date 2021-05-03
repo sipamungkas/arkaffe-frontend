989c0b4aa0de9856875812623de20cdea6ba88ea
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-03 22:22:31 +0700

    feat: fix chart section on dashboard

--- src/components/dashboard/chart/Chart.js
+++ src/components/dashboard/chart/Chart.js
@@ -1,5 +1,59 @@
+import { Bar } from "react-chartjs-2";
+
 import classes from "./Chart.module.css";
 
 export default function Chart() {
-  return <div className={classes.chart}></div>;
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "Income",
+        data: [0, 3000, 5000, 3000, 2000, 3000],
+        backgroundColor: "rgba(255, 186, 51, 1)",
+        stack: 1,
+      },
+      {
+        label: "Outcome",
+        data: [-3000, -5000, -3000, -2000, -3000, -1500],
+        backgroundColor: "rgba(106, 64, 41, 1)",
+        stack: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    barThickness: 16,
+    borderRadius: 5,
+    scales: {
+      yAxes: [
+        {
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          stacked: true,
+        },
+      ],
+    },
+  };
+
+  return (
+    <div className={classes.chart}>
+      <img
+        className={classes.more}
+        src="/assets/icons/more-icon.svg"
+        alt="more"
+      />
+      <div className="header">
+        <h1 className={classes.title}>Monthly Report</h1>
+        <h3 className={classes.duration}>Last 6 months</h3>
+      </div>
+      <Bar data={data} options={options} />
+    </div>
+  );
 }
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,27 +4,32 @@ import classes from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
-    <section className={`container ${classes.dashboard}`}>
-      <div className={classes["top-section"]}>
-        <h3 className={classes.title}>See how your store progress so far</h3>
-        <div className={classes["filter-container"]}>
-          <label className={classes.radio}>
-            <input type="radio" name="filter" />
-            Daily
-          </label>
-          <label className={classes.radio}>
-            <input type="radio" name="filter" />
-            Weekly
-          </label>
-          <label className={classes.radio}>
-            <input type="radio" name="filter" />
-            Monthly
-          </label>
+    <div className={classes.background}>
+      <section className={`container ${classes.dashboard}`}>
+        <div className={classes["top-section"]}>
+          <h3 className={classes.title}>See how your store progress so far</h3>
+          <div className={classes["filter-container"]}>
+            <label className={classes.radio}>
+              <input type="radio" name="filter" />
+              Daily
+            </label>
+            <label className={classes.radio}>
+              <input type="radio" name="filter" />
+              Weekly
+            </label>
+            <label className={classes.radio}>
+              <input type="radio" name="filter" />
+              Monthly
+            </label>
+          </div>
         </div>
-      </div>
-      <div className={classes.content}>
-        <Chart />
-      </div>
-    </section>
+        <div className={classes.content}>
+          <div className={`${classes["card"]} ${classes["chart-wrapper"]}`}>
+            <Chart />
+          </div>
+          <div className={classes["right-wrapper"]}>circle</div>
+        </div>
+      </section>
+    </div>
   );
 }
