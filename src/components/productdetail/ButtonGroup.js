6cfb8aa3c0f2ad95e47b7d32f3f31e320c672b15
Riski Arifani <rskrfni@gmail.com>
2021-05-04 11:15:08 +0700

    fix: product detail button chat

--- src/components/productdetail/ButtonGroup.js
+++ src/components/productdetail/ButtonGroup.js
@@ -1,15 +1,19 @@
 import React from "react";
 import classes from "./ButtonGroup.module.css";
 import { connect } from "react-redux";
+import { useHistory } from "react-router";
 
 function ButtonGroup(props) {
   const role = props.loginReducer.user.role;
+  const history = useHistory()
   return (
     <div className={classes["button-group"]}>
       <button className={classes["btn-cart"]} type="button">
         Add to Cart
       </button>
-      <button className={classes["btn-ask"]} type="button">
+      <button className={classes["btn-ask"]} type="button" onClick={()=> {
+        history.push("/chat")
+      }} >
         Ask a Staff
       </button>
       {role === 1 && (
